62b5ee1913b47b168618d1adeb63911d58cc75b0
Charmy Nitin Vora <charmy-nitin.vora@siemens.com>
2025-03-06 14:05:18 +0800

    optimized rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import CardList from "./components/CardList";
-import SearchBox from "./components/SearchBox";
+import Container from "./components/Container";
+import Header from "./components/Header";
 
 const App = () => {
-  const [searchString, setSearchString] = useState("");
-  const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilteredMonsters] = useState([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      const users = await response.json();
-      setMonsters(users);
-    };
-    fetchUsers();
-  }, []);
-
-  useEffect(() => {
-    setFilteredMonsters(
-      monsters.filter((monster) =>
-        monster.name
-          .toLocaleLowerCase()
-          .includes(searchString.toLocaleLowerCase())
-      )
-    );
-  }, [monsters, searchString]);
-
-  const handleSearchStringChange = (event) => {
-    setSearchString(event.target.value);
-  };
-
   return (
     <>
-      <h1>Monsters</h1>
-      <SearchBox
-        className="search-box"
-        placeHolder="search monsters"
-        handleSearchStringChange={handleSearchStringChange}
-      />
-      <CardList monsters={filteredMonsters}></CardList>
+      <Header />
+      <Container />
     </>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  console.log("render header");
+
+  return (
+    <>
+      <h1>Monsters Rolodox</h1>
+    </>
+  );
+};
+
+export default Header;
